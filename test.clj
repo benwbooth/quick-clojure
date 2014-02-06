a8c80e2d450b82c67d8d49179837c8c47b1e0392
Ben Booth <benwbooth@gmail.com>
2014-02-06 15:49:32 -0800

    better debugging fix stdin session bug

--- test.clj
+++ test.clj
@@ -1,3 +1,8 @@
-":"; exec ./quick lein exec "$0" "$@"
+#!/usr/bin/env quick-exec
 (println "Hello World!")
+(println "Press ENTER") (read-line)
+(println "Hello Again!")
+(println "Hello Again!")
+(println "Hello Again!")
+(println "Hello Again!")
 (println "Press ENTER") (read-line)
