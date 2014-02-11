d2e8897ef5983213ecd87b274bae5cecfb6cde63
Ben Booth <benwbooth@gmail.com>
2014-02-10 16:28:15 -0800

    re-add REPLOAD support

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name="quick-clojure",
-      version='0.9',
+      version='0.10',
       packages=find_packages(),
       # metadata for upload to PyPI
       author="Ben Booth",
