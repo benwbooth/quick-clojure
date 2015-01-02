7ed318961edf0899d38ffc40a8a1b2278be88260
Ben Booth <benwbooth@gmail.com>
2015-01-02 14:07:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name="quick-clojure",
-      version='0.12',
+      version='0.13',
       packages=find_packages(),
       # metadata for upload to PyPI
       author="Ben Booth",
